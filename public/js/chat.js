584f4754e890aefe0904744bcebe8bd24e944e92
Kristyna_Kohoutova <kristyna.kohoutova@tul.cz>
2022-04-24 09:06:30 +0200

    new version - with course EUR

--- public/js/chat.js
+++ public/js/chat.js
@@ -9,41 +9,159 @@ const $messages = document.querySelector('#messages')
 //templates
 const messageTemplate = document.querySelector('#message-template').innerHTML
 
-socket.on('message', (message) => {
-    console.log(message)
-    var html = ""
-    switch(message.messageType){
-        case "text":
-            html = Mustache.render(messageTemplate, {
-                message: message.text,
-                createdAt: moment(message.createdAt).format('k:mm a')
+const availableCommands =  [["what","name"], 
+                            ["what", "time"],
+                            ["what", "course", "eur"], 
+                            ["what", "history", "eur"], 
+                            ["help"]
+                        ]
+
+const dangerousKeyWords = ["eval", "setTimeout", "setInterval", "new Function", "script"]
+
+$messageFormButton.addEventListener('click', async function(e){
+    
+    sendMessageToServer()
+    e.preventDefault();
+})
+
+async function sendMessageToServer(){
+    var data = $messageFormInput.value
+    var filteredInput = filterUserInput(data)
+    console.log(filteredInput)
+    if(!(filteredInput == "Bad input")){
+        console.log(data)
+        const options = {
+            method: 'POST',
+            headers: {
+                "Content-Type" : "application/json"
+            }, 
+            body: JSON.stringify({"data": filteredInput})
+        }
+        try{
+            const response = await fetch('/', options).then(response => {
+                console.log(response)
+                return response
             })
-            break;
-        case "time":
-            html =  Mustache.render(messageTemplate, {
-                message: "It is currently " +moment(message.text).format('k:mm a, MMMM Do YYYY'),
-                createdAt: moment(message.createdAt).format('k:mm a')
-            })  
-            break;
-        default:
-            html = ""
-    }    
-    $messages.insertAdjacentHTML('beforeend', html)
-})
-
-$messageForm.addEventListener('submit', (e) => {
-    e.preventDefault()
-
-    $messageFormButton.setAttribute('disabled', 'disabled')
-    const message = e.target.elements.message.value
-
-    socket.emit('sendMessage', message, (message, answer) => {
-        $messageFormButton.removeAttribute('disabled')
-        $messageFormInput.value = ''
-        $messageFormInput.focus()
-
-        console.log('The message was delivered!', message)
+            const answer = await response.json()
+            console.log("in try")
+            console.log(answer.response.message)
+            responseToShow = processResponse(answer.response)
+            setMessageText(responseToShow)
         
+        }catch(e){
+            console.log("not send")
+            console.log(e)
+        }
+    }else{
+        setMessageText("Bad input, can not process this message")
+    }
+    
+    
+}
+
+function filterUserInput(input){
+    var foundCommands = 0
+    var foundCommand = ""
+    var isNotDangerous = true
+    dangerousKeyWords.forEach(word => {
+        if(input.includes(word)){
+            isNotDangerous = false
+            console.log(word)
+        }
     })
-})
+    if(!input == "" && isNotDangerous){
+        input = input.toLowerCase()
+        console.log(isNotDangerous)
+        availableCommands.forEach(command => {
+            var currentCommand = true
+            command.forEach(keyWord => {
+                if(!input.includes(keyWord)){
+                    currentCommand = false
+                }
+            })
+            if(currentCommand){
+                command.forEach(word => {
+                    foundCommand += word
+                })
+                foundCommands++
+            }
+        })
+        if(foundCommands != 1){
+            return "Bad input"
+        }else{
+            return foundCommand
+        }
+    }else{
+        return "Bad input"
+    }
+}
+
+function setMessageText(messageBody){
+    var html = ""
+    html = Mustache.render(messageTemplate, {
+        message: messageBody,
+        // createdAt: moment(message.createdAt).format('k:mm a')
+    })
+    $messages.insertAdjacentHTML('beforeend', html)
+    $messageFormInput.value = ''
+    $messageFormInput.focus()
+
+}
+
+function processResponse(resObj){
+    if(resObj.messageType == "text"){
+        return resObj.message
+    }else if(resObj.messageType == "time"){
+        return "It is currently " +moment(resObj.message).format('k:mm a, MMMM Do YYYY')
+    }else if(resObj.messageType == "table"){
+        return "not supported yet"
+    }else{
+        return "not supported message type"
+    }
+}
+
+// socket.on('message', (message) => {
+//     console.log(message)
+//     var html = ""
+//     switch(message.messageType){
+//         case "text":
+//             html = Mustache.render(messageTemplate, {
+//                 message: message.text,
+//                 createdAt: moment(message.createdAt).format('k:mm a')
+//             })
+//             break;
+//         case "time":
+//             html =  Mustache.render(messageTemplate, {
+//                 message: "It is currently " +moment(message.text).format('k:mm a, MMMM Do YYYY'),
+//                 createdAt: moment(message.createdAt).format('k:mm a')
+//             })  
+//             break;
+
+//         case "help":
+//             html = Mustache.render(messageTemplate, {
+//                 message: message.text,
+//                 createdAt: moment(message.createdAt).format('k:mm a')
+//             })
+//             break;
+//         default:
+//             html = ""
+//     }    
+//     $messages.insertAdjacentHTML('beforeend', html)
+// })
+
+// $messageForm.addEventListener('submit', (e) => {
+//     e.preventDefault()
+
+//     $messageFormButton.setAttribute('disabled', 'disabled')
+//     const message = e.target.elements.message.value
+
+//     socket.emit('sendMessage', message, (message, answer) => {
+//         $messageFormButton.removeAttribute('disabled')
+//         $messageFormInput.value = ''
+//         $messageFormInput.focus()
+
+//         console.log('The message was delivered!', message)
+        
+//     })
+// })
 
--- src/script.js
+++ src/script.js
@@ -1,9 +1,12 @@
 const path = require('path')
 const http = require('http')
+const https = require('https')
+const fs = require('fs')
 const express = require('express')
 const socketio = require('socket.io')
 const { generateMessage } = require('./utils/messages')
 
+const striptags = require('striptags')
 const app = express()
 const server = http.createServer(app)
 const io = socketio(server)
@@ -11,41 +14,112 @@ const io = socketio(server)
 const port = process.env.PORT || 3000
 const publicDirectoryPath = path.join(__dirname, '../public')
 
-app.use(express.static(publicDirectoryPath))
-
-var messageType = "text"
-io.on('connection', (socket) => {
-    console.log('New WebSocket connection')
-    socket.emit('message', generateMessage('Welcome!', "text"))
-
-    socket.on('sendMessage', (message, callback) => {
-
-        const processedMessage = processMessage(message)
-
-        socket.emit('message', generateMessage(processedMessage, messageType))
-        callback(processedMessage) 
-        messageType = "text"  
-    })
-    socket.on('disconnect', () => {
-        socket.emit('message', generateMessage('A user has left!', "text"))
-    })
-
-})
-
 server.listen(port, () => {
     console.log(`Server is up on port ${port}!`)
 })
 
-function processMessage(message){
+app.use(express.static(publicDirectoryPath))
+app.use(express.json({limit: '1mb'}))
 
-    if(message.includes("what") && message.includes("name")){
-        return ('My name is ChatBot007 :D')
-    }
-    else if(message.includes("what") && message.includes("time")){
-        const time =  new Date().getTime()
-        messageType = "time"
-        return (time)
-    }
+var messageType = "text"
+app.post('/', async (req, res) => {
+    var answer = ""
 
-    return (message)
+    answer = processMessage(req)
+    var generatedMessage = generateMessage(answer, messageType)
+    console.log(generatedMessage)
+    res.json({
+        response: generatedMessage
+    }) 
+    messageType = "text"
+})
+
+
+function processMessage(req){
+    var messageToAnswer = "default message :D"
+    console.log()
+    var recievedMessage = req.body.data
+    recievedMessage = striptags(String(recievedMessage))
+    console.log("recievedMessage")
+    console.log(recievedMessage)
+    switch(recievedMessage){
+        case "whatname":
+            messageToAnswer = "My name is ChatBot007"
+            break;
+        case "whattime":
+            const time =  new Date().getTime()
+            messageToAnswer = time
+            messageType = "time"
+            break;
+        case "help":
+            messageToAnswer = "Available commands: what name, what time, what course EUR, what history EUR and help"
+            break;
+        case "whatcourseeur":
+            dowloadFile()
+            var parsedData = readFromDownloadedFile("downloadedFile.txt", "EUR")
+            messageToAnswer = "EUR " + parsedData[1] + " " + parsedData[0]
+            break;
+        case "whathistoryEUR":
+            messageToAnswer = "History is not supported yet"
+            messageType = "table"
+            break;
+        default: 
+            messageToAnswer = "from default"
+    }
+    return messageToAnswer 
+}
+
+function dowloadFile(){
+    const file = fs.createWriteStream("./downloadedFile.txt")
+    dataUrl = getURL("https://www.cnb.cz/cs/financni-trhy/devizovy-trh/kurzy-devizoveho-trhu/kurzy-devizoveho-trhu/denni_kurz.txt?date=")
+    const request = https.get(dataUrl, 
+        function(response){
+            response.pipe(file)
+            file.on("finish", () => {
+                file.close()
+                console.log("download completed")
+            })
+        })
+}
+
+function getURL(urlAdress){
+    var d = new Date()
+    var year = d.getFullYear()
+    var month = d.getMonth() + 1
+    var day = d.getDate()
+    var finalDate = urlAdress
+    finalDate += (day < 10) ? "0" + day.toString() + ".": day.toString() + "."
+    finalDate += (month < 10) ? "0" + month.toString() + "." : month.toString() + "."
+    finalDate += year.toString()
+    console.log(finalDate) 
+    return finalDate
+}
+
+function readFromDownloadedFile(filename, cName){
+    var toReturn = ["nothing"]
+    var readData = fs.readFileSync("./src/downloadedFile.txt", 'utf-8', (err, data) => {
+        if(err){
+            console.error(err)
+            return
+        }
+    })
+    var parsedData = parseFileData(readData)
+    for(i = 0; i < parsedData[1].length; i++){
+        if(parsedData[1][i].includes(cName)){
+            toReturn = [parsedData[0], parsedData[1][i][4]]
+        }
+    }
+    return toReturn
+}
+
+function parseFileData(data){
+    var date = ""
+    var dataArray = []
+    var splittedData = data.split("\n")
+    date = (splittedData[0].split(" "))[0]
+    for(i = 2; i < splittedData.length -1; i++){
+        var splitted = splittedData[i].split("|")
+        dataArray.push(splitted)
+    }
+    return [date, dataArray]
 }
